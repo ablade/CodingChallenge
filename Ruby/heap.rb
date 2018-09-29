dfed48132015cde0c2ea88e553cb114bb3e498be
Alvin <baldea98@uw.edu>
2018-09-29 15:49:52 -0700

    Modified ruby heap to include min_heapify & sorting in decreasing order

--- Ruby/heap.rb
+++ Ruby/heap.rb
@@ -42,10 +42,10 @@ def build_max_heap(a)
   end
 end
 
-def heapSort(a)
+def increasingHeapSort(a)
   s = a.length-1
   al = s-1
-  for i in al.downto(2)
+  for i in a.length.downto(2)
     t = a[s]
     a[s] = a[1] #Place largest at the end
     a[1] = t
@@ -55,9 +55,61 @@ def heapSort(a)
   end
 end
 
-  array = [0,5,7,2,-11,0,2,34,-3,4,67,-21]
-  puts "ORIGINAL : #{array}"
-  build_max_heap(array)
-  puts "MAX HEAP : #{array}"
-  heapSort(array)
-  puts "SORTED   : #{array}"
+def min_heapify(the_array, the_last_valid_index, node_index)
+  a = the_array
+  lvi = the_last_valid_index
+  i = node_index
+  l = Left(i)
+  r = Right(i)
+  smallest = i
+  if l <= lvi and a[l] < a[smallest]
+    smallest = l
+  end
+  if r <= lvi and a[r] < a[smallest]
+    smallest = r
+  end
+  if smallest != i
+    temp = a[i]
+    a[i] = a[smallest]
+    a[smallest] = temp
+    min_heapify(a,lvi,smallest)
+  end
+end
+
+def build_min_heap(the_array)
+
+  low_half = the_array.length / 2
+  for i in low_half.downto(1)
+    min_heapify(the_array,the_array.length-1,i)
+  end
+end
+
+def decreasingHeapSort(the_array)
+  build_min_heap(the_array)
+
+  last_valid_ptr = the_array.length - 1
+
+  for does_not_matter in the_array.length.downto(2)
+    #swap the first with the last and min_heapify
+    t = the_array[last_valid_ptr]
+    the_array[last_valid_ptr] = the_array[1]
+    the_array[1] = t
+    last_valid_ptr -= 1
+    min_heapify(the_array,last_valid_ptr,1)
+  end
+
+
+end
+
+
+  array1 = [0,5,7,2,-11,0,2,34,-3,4,67,-21]
+  array2 = [0,5,7,2,-11,0,2,34,-3,4,67,-21]
+  puts "ORIGINAL : #{array1}"
+  build_min_heap(array1)
+  puts "MIN HEAP : #{array1}"
+  decreasingHeapSort(array1)
+  puts "H SORTING : #{array1}"
+  build_max_heap(array2)
+  puts "MAX HEAP : #{array2}"
+  increasingHeapSort(array2)
+  puts "SORTED   : #{array2}"
